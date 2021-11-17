cf262f2a706392aa8715fcf07105a30a2186f94a
jdonald24 <endershadow128@gmail.com>
2021-11-17 15:37:57 -0600

    added the card and Move class

---
+++ OperationPokemon/src/main/java/dmacc/beans/Card.java
@@ -0,0 +1,35 @@
+package dmacc.beans;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Austin Donald - ajdonald
+ * CIS175 - Fall 2021
+ * Nov 17, 2021
+ */
+@Entity
+@Data
+@NoArgsConstructor
+public class Card {
+	@Id
+	@GeneratedValue
+	private long id;
+	private double price;
+	private String name;
+	private int hp;
+	private String pokemonType;
+	private boolean isCardJumbo;
+	private String cardType;
+	private boolean isGX;
+	private boolean isEX;
+	private boolean isMega;
+	List <Move> moveListl;
+	private String ability;
+}
