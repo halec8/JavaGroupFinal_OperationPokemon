c465d9806ff044f1525cc91bc074a04baa2c0bbe
jdonald24 <endershadow128@gmail.com>
2021-11-21 20:04:04 -0600

    tried to resolve conflicts

--- OperationPokemon/src/main/java/dmacc/controller/WebController.java
+++ OperationPokemon/src/main/java/dmacc/controller/WebController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import dmacc.beans.Seller;
 import dmacc.repository.OperationPokemonRepository;
 
@@ -60,7 +59,6 @@ public class WebController {
         return viewAllSellers(model);
 	}
 	// end of the seller methods
-
 
 }
 
