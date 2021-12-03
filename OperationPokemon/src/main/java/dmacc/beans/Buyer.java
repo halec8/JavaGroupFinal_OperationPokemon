853a999cc7f4f010fba39845b2e7825106df4084
AndrewJhD <89947450+AndrewJhD@users.noreply.github.com>
2021-12-02 21:05:25 -0600

    adding drop downs and making fixes

--- OperationPokemon/src/main/java/dmacc/beans/Buyer.java
+++ OperationPokemon/src/main/java/dmacc/beans/Buyer.java
@@ -28,58 +28,10 @@ public class Buyer {
 	
 	private String name;
 	
-	private String relationship;
-	
 	private String street;
 	
 	private String city;
 	
 	private String state;
 
-	
-		public Buyer(String name) {
-		super();
-		this.name = name;
-		
-		}
-		public Buyer(String name, String relationship) {
-		super();
-		this.name = name;
-		this.relationship = relationship;
-		}
-		public Buyer(int id, String name, String relationship) {
-
-			this.id = id;
-			this.name = name;
-			this.relationship = relationship;
-		}
-		
-		public Buyer(int id, String name, String relationship, String street) {
-
-			this.id = id;
-			this.name = name;
-			this.relationship = relationship;
-			this.street = street;
-		
-		}
-		
-		public Buyer(int id, String name, String relationship, String street, String city) {
-
-			this.id = id;
-			this.name = name;
-			this.relationship = relationship;
-			this.street = street;
-			this.city = city;
-		
-		}
-	
-		public Buyer(int id, String name, String relationship, String street, String city, String state) {
-
-			this.id = id;
-			this.name = name;
-			this.relationship = relationship;
-			this.street = street;
-			this.city = city;
-			this.state = state;
-		}
 }
